21830e4699e97691fad4554d5f96136bda0a7a5e
sagar <rsaagar7200@gmail.com>
2023-02-28 14:06:38 +0545

    profile right bar friend list updates

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -5,7 +5,7 @@ export default function Topbar() {
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
-        <span className="logo"> HALOsocial</span>
+        <span className="logo"> Gosocial</span>
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
