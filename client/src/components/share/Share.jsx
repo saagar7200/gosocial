49f5e20eee6cb3a19931b4d125ff396e32392589
sagar <rsaagar7200@gmail.com>
2023-03-08 00:31:34 +0545

    uploaded images

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -1,5 +1,11 @@
 import "./share.css";
-import { PermMedia, Label, Room, EmojiEmotions } from "@material-ui/icons";
+import {
+  PermMedia,
+  Label,
+  Room,
+  EmojiEmotions,
+  CancelOutlined,
+} from "@material-ui/icons";
 import { useContext, useRef, useState } from "react";
 import { AuthContext } from "../../context/userContext/AuthContext";
 import { Link } from "react-router-dom";
@@ -10,22 +16,40 @@ import { PostContext } from "../../context/postContext/PostContext";
 const Share = ({ username }) => {
   const PF = process.env.REACT_APP_PUBLIC_FOLFER;
   const { user } = useContext(AuthContext);
-  // const [file, setFile] = useState();
+  const [file, setFile] = useState(null);
   const postTxt = useRef();
   const { dispatch } = useContext(PostContext);
 
+  const handleChangeFile = (e) => {
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        setFile(reader.result);
+      }
+    };
+    reader.readAsDataURL(e.target.files[0]);
+  };
+
   const handlePost = async (e) => {
     e.preventDefault();
+    const postData = new FormData();
+    postData.set("desc", postTxt.current.value);
+    postData.set("userId", user._id);
+    postData.set("img", file);
+
     try {
-      await axios.post(`/post`, {
-        desc: postTxt.current.value,
-        userId: user._id,
+      await axios.post(`/post`, postData, {
+        headers: {
+          accept: "application/json",
+          "Accept-Language": "en-US,en;q=0.8",
+          "Content-Type": `multipart/form-data`,
+        },
       });
       postTxt.current.value = "";
+      setFile(null);
       getPost(username, user._id, dispatch);
-    } catch (err) {
-      console.log("share post ", err.message);
-    }
+    } catch (err) {}
   };
 
   return (
@@ -46,12 +70,23 @@ const Share = ({ username }) => {
           />
         </div>
         <hr className="shareHr" />
+        {file && (
+          <div className="previewContainer">
+            <img className="previewImage" src={file} alt="preview" />
+            <CancelOutlined className="cancel" onClick={() => setFile(null)} />
+          </div>
+        )}
         <form className="shareBottom" onSubmit={handlePost}>
           <div className="shareOptions">
             <label className="shareOption">
               <PermMedia htmlColor="tomato" className="shareIcon" />
               <span className="shareOptionText">Photo or Video</span>
-              <input style={{ display: "none" }} type={"file"} />
+              <input
+                style={{ display: "none" }}
+                type={"file"}
+                onChange={handleChangeFile}
+                multiple={false}
+              />
             </label>
             <div className="shareOption">
               <Label htmlColor="blue" className="shareIcon" />
@@ -66,6 +101,7 @@ const Share = ({ username }) => {
               <span className="shareOptionText">Feelings</span>
             </div>
           </div>
+
           <button className="shareButton" type="submit">
             Share
           </button>
